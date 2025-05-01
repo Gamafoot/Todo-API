5174712117722c6d33e352868e3c1c309c87db95
Gamafoot <gamafootsas@gmail.com>
2025-05-01 17:27:30 +0700

    fix is_owned_column

--- src/assets/migration/func_is_owned_column.sql
+++ src/assets/migration/func_is_owned_column.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE FUNCTION public.is_owned_column(p_user_id integer, p_column_id integer)
 RETURNS boolean
+LANGUAGE plpgsql
 AS $function$
 BEGIN
 	RETURN EXISTS (
-        SELECT 1 FROM tasks INNER JOIN columns ON columns.id = tasks.column_id 
-        INNER JOIN projects ON projects.id = columns.id 
-        WHERE projects.user_id = p_user_id 
+        SELECT 1 FROM columns
+        INNER JOIN projects ON projects.id = columns.project_id
+        WHERE projects.user_id = p_user_id AND columns.id = p_column_id
         LIMIT 1
     );
 END;
-$function$ LANGUAGE plpgsql
+$function$
