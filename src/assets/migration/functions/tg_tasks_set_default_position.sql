58361ea44118aa8e27b792a8e59f278fc13c0f1d
Gamafoot <gamafootsas@gmail.com>
2025-05-11 18:22:54 +0700

    fix create task

--- src/assets/migration/functions/tg_tasks_set_default_position.sql
+++ src/assets/migration/functions/tg_tasks_set_default_position.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION public.tg_tasks_set_default_position()
 RETURNS TRIGGER AS $$
 BEGIN
-    PERFORM public.tasks_set_default_position(NEW.column_id, NEW.id);
+    PERFORM public.tasks_set_default_position(NEW.column_id::integer, NEW.id::integer);
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
