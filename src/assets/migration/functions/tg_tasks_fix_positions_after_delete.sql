294f9f7d764c3ee6e31f1832ed15ce12ac772ec2
Gamafoot <gamafootsas@gmail.com>
2025-05-21 22:19:27 +0700

    fix tasks_fix_position...

--- src/assets/migration/functions/tg_tasks_fix_positions_after_delete.sql
+++ src/assets/migration/functions/tg_tasks_fix_positions_after_delete.sql
@@ -2,5 +2,6 @@ CREATE OR REPLACE FUNCTION tg_tasks_fix_positions_after_delete()
 RETURNS TRIGGER AS $$
 BEGIN
     PERFORM public.tasks_fix_positions_after_delete(OLD.column_id, OLD.position);
+    RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
