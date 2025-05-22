0598853a3ac3f7093148f0256337b03541781ab9
Gamafoot <gamafootsas@gmail.com>
2025-05-22 14:52:26 +0700

    fix position

--- src/assets/migration/functions/tasks_move_to_column.sql
+++ src/assets/migration/functions/tasks_move_to_column.sql
@@ -15,11 +15,10 @@ BEGIN
         RETURN;
     END IF;
 
-    PERFORM tasks_set_default_position(p_new_column_id, p_task_id);
-    
     UPDATE tasks SET column_id = p_new_column_id
     WHERE id = p_task_id;
-
+    
+    PERFORM tasks_set_default_position(p_new_column_id, p_task_id);
     PERFORM tasks_move_to_position(p_new_column_id, p_task_id, p_new_position);
     PERFORM tasks_fix_positions_after_delete(v_old_column_id, v_old_position);
 END;
